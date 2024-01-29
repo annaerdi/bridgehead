139dabab878eab12085fc580441ba30dae7383d1
AnnaErdi <anna.erdi@ait.ac.at>
2024-01-29 22:49:12 +0100

    added debugger

--- lib.rs
+++ lib.rs
@@ -12,6 +12,7 @@ pub mod phonebook_contract {
     impl PhoneBook {
         #[ink(constructor)]
         pub fn new() -> Self {
+            ink::env::debug_println!("created new instance at {}", Self::env().block_number());
             Self { contacts: Mapping::default() }
         }
 
@@ -21,34 +22,51 @@ pub mod phonebook_contract {
         }
 
         #[ink(message)]
-        pub fn get_contact(&self, account_id: AccountId) -> Option<AccountId> {
-            self.contacts.get(&account_id)
+        pub fn get_contact(&self, account_id: AccountId) -> AccountId {
+            self.contacts.get(&account_id).unwrap()
         }
+
+        #[ink(message)]
+        fn print(&self) {
+            let caller = self.env().caller();
+            let message = ink_prelude::format!("got a call from {:?}", caller);
+            ink::env::debug_println!(&message);
+        }
+
+
     }
 
     #[cfg(test)]
     mod tests {
         use super::*;
 
-        #[ink::test]
-        fn new_creates_empty_phone_book() {
-            let phonebook = PhoneBook::new();
-            let account: AccountId = AccountId::from([0xFF as u8; 32]);
-            // Initially, there should be no contact for this account
-            assert_eq!(phonebook.get_contact(account), None);
-        }
+        // #[ink::test]
+        // fn new_creates_empty_phone_book() {
+        //     let phonebook = PhoneBook::new();
+        //     let account: AccountId = AccountId::from([0xFF as u8; 32]);
+        //     // Initially, there should be no contact for this account
+        //     assert_eq!(phonebook.get_contact(account), Option<_>);
+        // }
 
         #[ink::test]
         fn add_and_get_contact_works() {
             let mut phonebook = PhoneBook::new();
             let account: AccountId = [0x01; 32].into();
-            let contact: AccountId = [0x02; 32].into();
+            let contact1: AccountId = [0x02; 32].into();
+            let contact2: AccountId = [0x02; 32].into();
+
+            println!("{:?}", contact1);
+
 
             // Add a contact
-            phonebook.add_contact(account, contact);
+            phonebook.add_contact(account, contact1);
+            phonebook.add_contact(account, contact2);
+
+            println!("{:?}", phonebook.get_contact(account));
 
             // Now, the contact should be retrievable
-            assert_eq!(phonebook.get_contact(account), Some(contact));
+            assert_eq!(phonebook.get_contact(account), contact1);
+            assert_eq!(phonebook.get_contact(account), contact2);
         }
     }
 
