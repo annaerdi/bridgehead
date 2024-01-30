ae9344dd4bc90e0c71ba12111a1636d8133cbd9f
annaerdi <erdianna607@gmail.com>
2024-01-30 13:54:13 +0100

    changed storage to map account id to a vector of account ids

--- lib.rs
+++ lib.rs
@@ -3,10 +3,12 @@
 #[ink::contract]
 pub mod phonebook_contract {
     use ink::storage::Mapping;
+    use ink_prelude::vec::Vec;
+
 
     #[ink(storage)]
     pub struct PhoneBook {
-        contacts: Mapping<AccountId, AccountId>,
+        contacts: Mapping<AccountId, Vec<AccountId>>,
     }
 
     impl PhoneBook {
@@ -18,61 +20,93 @@ pub mod phonebook_contract {
 
         #[ink(message)]
         pub fn add_contact(&mut self, account: AccountId, contact: AccountId) {
-            self.contacts.insert(account, &contact);
+            let mut contacts = self.contacts.get(&account).unwrap_or_default();
+            contacts.push(contact);
+            self.contacts.insert(account, &contacts);
         }
+
 
         #[ink(message)]
-        pub fn get_contact(&self, account_id: AccountId) -> Option<AccountId> {
-            self.contacts.get(&account_id)
+        pub fn get_contacts(&self, account: AccountId) -> Vec<AccountId> {
+            self.contacts.get(&account).unwrap_or_default()
         }
-
-
-        /// Get address for specific name.
-        #[ink(message)]
-        pub fn get_address(&self, name: AccountId) -> AccountId {
-            self.get_address_or_default(name)
-        }
-
-        /// Returns the address given the hash or the default address.
-        fn get_address_or_default(&self, name: AccountId) -> AccountId {
-            self.contacts
-                .get(name)
-                .unwrap_or(name)
-        }
-
 
     }
+
+
+    // #[ink(message)]
+    //     pub fn get_contact(&self, account_id: AccountId) -> Option<AccountId> {
+    //         self.contacts.get(&account_id)
+    //     }
+    //
+    //
+    //     /// Get address for specific name.
+    //     #[ink(message)]
+    //     pub fn get_address(&self, name: AccountId) -> AccountId {
+    //         self.get_address_or_default(name)
+    //     }
+    //
+    //     /// Returns the address given the hash or the default address.
+    //     fn get_address_or_default(&self, name: AccountId) -> AccountId {
+    //         self.contacts
+    //             .get(name)
+    //             .unwrap_or(name)
+    //     }
+
+
+
 
     #[cfg(test)]
     mod tests {
         use super::*;
 
         #[ink::test]
-        fn new_creates_empty_phone_book() {
-            let phonebook = PhoneBook::new();
-            let non_existent_account: AccountId = [0x00; 32].into();
-            assert_eq!(phonebook.get_contact(non_existent_account), None);
-        }
-
-        #[ink::test]
-        fn add_and_get_contact_works() {
+        fn add_and_get_contacts_works() {
             let mut phonebook = PhoneBook::new();
             let account: AccountId = [0x01; 32].into();
             let contact1: AccountId = [0x02; 32].into();
-            let contact2: AccountId = [0x02; 32].into();
+            let contact2: AccountId = [0x03; 32].into();
 
-            println!("{:?}", contact1);
-
-
-            // Add a contact
+            // Add two contacts to the same account
             phonebook.add_contact(account, contact1);
             phonebook.add_contact(account, contact2);
 
-            println!("{:?}", phonebook.get_contact(account));
+            // Retrieve the contacts for the account
+            let retrieved_contacts = phonebook.get_contacts(account);
+            println!("{:?}", retrieved_contacts);
 
-            // Now, the contact should be retrievable
-            assert_eq!(phonebook.get_contact(account), Some(contact1));
-            assert_eq!(phonebook.get_contact(account), Some(contact2)); // if i dont put some around it, i get the error that it expected `Option<AccountId>`, found `AccountId`
+            // Check that both contacts are in the retrieved list
+            assert_eq!(retrieved_contacts, vec![contact1, contact2]);
         }
+
+
+        // #[ink::test]
+        // fn new_creates_empty_phone_book() {
+        //     let phonebook = PhoneBook::new();
+        //     let non_existent_account: AccountId = [0x00; 32].into();
+        //     assert_eq!(phonebook.get_contact(non_existent_account), None);
+        // }
+
+        // #[ink::test]
+        // fn add_and_get_contact_works() {
+        //     let mut phonebook = PhoneBook::new();
+        //     let account: AccountId = [0x01; 32].into();
+        //     let contact1: AccountId = [0x02; 32].into();
+        //     let contact2: AccountId = [0x03; 32].into();
+        //
+        //     // Add two contacts to the same account
+        //     phonebook.add_contact(account, contact1);
+        //     phonebook.add_contact(account, contact2);
+        //
+        //     // Retrieve the contact for the account
+        //     let retrieved_contact = phonebook.get_contacts(account);
+        //
+        //     // Check that the retrieved contact is the last one added
+        //     assert_eq!(retrieved_contact, Some(contact2));
+        //
+        //     // The first contact (contact1) should have been overwritten by contact2
+        // }
+
     }
+
 }
